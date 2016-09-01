1750f0a9f4df1323b5e2f86295b87221ffdf281e
wyw.wang <wyw.wang@qunar.com>
2016-09-01 18:03:11 +0800

    mod

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,6 +1,25 @@
 var nodeAnnotation = require('node-annotation'),
     path = require('path');
 
+
+/**
+ * 配置全局错误处理
+ * [Function] 错误处理函数
+ */
+nodeAnnotation.setGlobalErrorHandler(function(err){
+  console.error(err);
+});
+
+/**
+ * 配置node－annotation内的日志流出
+ * [Boolean] 是否开启日志，默认true
+ * [String] "error/warn/info/log" 输出日至级别，默认warn
+ * [Function/LoggerObject] 日志处理函数或对象(类似log4js的Logger对象)，默认为console
+ */
+nodeAnnotation.setLogger(true, 'error', function(str, level) {
+    console.error('[ERROR]', str);
+});
+
 nodeAnnotation.start(path.resolve(__dirname, 'src'), function(){
     var app = require('./app');
     nodeAnnotation.app(app);
