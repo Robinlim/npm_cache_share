a70be16d3f90079f1618a0c305e05f6d34e08eb3
wyw.wang <wyw.wang@qunar.com>
2016-09-01 18:22:57 +0800

    fix

--- src/js/command/server.js
+++ src/js/command/server.js
@@ -20,7 +20,7 @@ var app = path.join(__dirname, '../app');
 module.exports = {
     run: function(opts) {
         var pm2 = which('pm2');
-        var env =_.extend({
+        var env = _.extend({
             port: opts.port || '8888'
         }, process.env);
         // 没有pm2或者指定了useFork就使用fork子进程方式
@@ -33,7 +33,7 @@ module.exports = {
         // 使用pm2管理server
             console.info(
                 'Will start server with PM2. \n' +
-                '- If you want to just start with fork ,use --useFork option. \n' +
+                '- If you want to just start with fork ,use "--useFork true" option. \n' +
                 '- You can append actions like "start/stop/list" and options like "--instances 4,etc." after.\n'
             );
             var action = opts[1] || 'start';  // 指令的第二个参数标示pm2的动作，默认start
