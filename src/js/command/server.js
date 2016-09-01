f1a56bf273324ac8e0fa1f79f806c397facb41de
wyw.wang <wyw.wang@qunar.com>
2016-09-01 18:09:12 +0800

    注释

--- src/js/command/server.js
+++ src/js/command/server.js
@@ -23,19 +23,21 @@ module.exports = {
         var env =_.extend({
             port: opts.port || '8888'
         }, process.env);
+        // 没有pm2或者指定了useFork就使用fork子进程方式
         if (!pm2 || opts.useFork) {
             console.info('Connot find pm2, will start server with fork.');
             childProcess.fork(app, {
                 env: env
             })
         } else {
+        // 使用pm2管理server
             console.info(
                 'Will start server with PM2. \n' +
                 '- If you want to just start with fork ,use --useFork option. \n' +
                 '- You can append actions like "start/stop/list" and options like "--instances 4,etc." after.\n'
             );
-            var action = opts[1] || 'start';
-            var options =[];
+            var action = opts[1] || 'start';  // 指令的第二个参数标示pm2的动作，默认start
+            var options =[]; // 指令中的_，数字以及port参数之外的参数全部传给pm2
             _.forIn(opts, function(value ,key){
                 if(key !== 'port' && key!== '_' && _.isNaN(_.toNumber(key))){
                     options.push((key.length==1?'-':'--')+key+' '+value);
